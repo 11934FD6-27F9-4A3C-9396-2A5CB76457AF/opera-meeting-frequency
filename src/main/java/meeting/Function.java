a74a17b543fa45d9ff01c80033aef86a2ef17af0
NikitaOmega <nikita.berezkin@omegapoint.se>
2025-04-17 10:05:35 +0200

    changed cron job to 9

--- src/main/java/meeting/Function.java
+++ src/main/java/meeting/Function.java
@@ -13,7 +13,7 @@ public class Function {
     public void weeklyRepost(
             final @TimerTrigger(
                     name = "weeklyAlertTrigger",
-                    schedule = "0 0 10 * * 4"
+                    schedule = "0 0 9 * * 4"
             ) String timerInfo,
             ExecutionContext context) {
         Logger logger = context.getLogger();
